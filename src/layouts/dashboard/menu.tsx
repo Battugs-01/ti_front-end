544e54496a6b3ea01fba861e6cddd42643653e1a
Bayarmunkh <b210910007@must.edu.mn>
2024-08-07 16:55:19 +0800

    update

--- src/layouts/dashboard/menu.tsx
+++ src/layouts/dashboard/menu.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { menuItems } from "./menu_items";
 
 const triangleStyle: React.CSSProperties = {
@@ -8,7 +8,7 @@ const triangleStyle: React.CSSProperties = {
   borderWidth: "0 12px 12px 12px",
   borderColor: "transparent transparent #E7EDEE transparent",
   position: "absolute",
-  top: "145%",
+  top: "85%",
   left: "55%",
   transform: "translateX(-50%)",
 };
@@ -17,14 +17,22 @@ export const Menu: React.FC = () => {
     <div className="flex items-center">
       {menuItems.map((item, index) => (
         <div key={index} className="flex items-center relative p-6">
-          <Link
+          <NavLink
             to={item.path}
-            className="flex items-center gap-2 text-primary-300 no-underline"
+            className={({ isActive }) => {
+              return `flex items-center gap-2 text-primary-300 no-underline
+            ${isActive && "text-primary-600"}      
+            `;
+            }}
           >
-            {item.icon}
-            <div>{item.name}</div>
-          </Link>
-          <div style={triangleStyle}></div>
+            {({ isActive }) => (
+              <>
+                {item.icon}
+                <div>{item.name}</div>
+                {isActive && <div style={triangleStyle}></div>}
+              </>
+            )}
+          </NavLink>
         </div>
       ))}
     </div>
