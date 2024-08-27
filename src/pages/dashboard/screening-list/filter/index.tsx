8b712913fdf1eb10b53f2d48e0325ba451478646
Bayarmunkh <b210910007@must.edu.mn>
2024-08-27 10:23:29 +0800

    updtae

--- src/pages/dashboard/screening-list/filter/index.tsx
+++ src/pages/dashboard/screening-list/filter/index.tsx
@@ -1,7 +1,10 @@
 import ProForm, { ProFormSelect, ProFormText } from "@ant-design/pro-form";
-import { Button, Divider } from "antd";
+import { useRequest } from "ahooks";
+import { Avatar, Button, Divider } from "antd";
 import { agencyArray, levelOptions } from "config";
 import { FormattedMessage, useIntl } from "react-intl";
+import file from "service/file";
+import permission from "service/settings/permission";
 
 interface ScreeningListFilterType {
   onFinish?: (formData: Record<string, any>) => Promise<boolean | void>;
@@ -11,6 +14,10 @@ export const ScreeningListFilter: React.FC<ScreeningListFilterType> = ({
   onFinish,
 }) => {
   const intl = useIntl();
+  const personChargeList = useRequest(permission.list, {
+    manual: true,
+  });
+
   return (
     <ProForm
       onFinish={onFinish}
@@ -69,6 +76,20 @@ export const ScreeningListFilter: React.FC<ScreeningListFilterType> = ({
       />
       <ProFormSelect
         name="person_in_charge_ids"
+        request={async () => {
+          const data = await personChargeList.runAsync({});
+          return data?.items?.map((val) => ({
+            label: (
+              <div className="flex items-center gap-3">
+                <Avatar src={file.fileToUrl(val?.profile?.physical_path || "")}>
+                  {val?.first_name?.substring(0, 2)}
+                </Avatar>
+                <div>{val?.first_name}</div>
+              </div>
+            ),
+            value: val.id,
+          }));
+        }}
         placeholder={intl.formatMessage({ id: "person_in_charge" })}
         fieldProps={{
           mode: "multiple",
