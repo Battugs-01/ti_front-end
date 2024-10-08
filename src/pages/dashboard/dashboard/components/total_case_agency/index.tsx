9588f5dcdb41bf5875fb350f441dfcb427638958
Battugs Choijil <battugs@fibo.cloud>
2024-10-08 14:39:11 +0800

    [sec]

--- src/pages/dashboard/dashboard/components/total_case_agency/index.tsx
+++ src/pages/dashboard/dashboard/components/total_case_agency/index.tsx
@@ -1,11 +1,12 @@
+import Pin from "assets/img/Pin.png";
 import { ICard } from "components/card";
 import { FormattedMessage } from "react-intl";
 import { TotalPointInterface } from "service/dashboard/type";
-// import Map from "components/live-map/index.jsx";
 
 interface TotalCaseAgencyProps {
   data?: TotalPointInterface[];
 }
+
 export const TotalCaseAgency: React.FC<TotalCaseAgencyProps> = (data) => {
   return (
     <ICard xR yR>
@@ -13,7 +14,34 @@ export const TotalCaseAgency: React.FC<TotalCaseAgencyProps> = (data) => {
         <FormattedMessage id="total_case_agency" />
       </p>
 
-      {/* <Map data={{}} /> */}
+      <div
+        style={{
+          backgroundImage: "url('/svg/mn_map1.svg')",
+          backgroundPosition: "top", // Positions the image in the center
+          backgroundRepeat: "no-repeat", // Prevents the image from repeating
+          backgroundSize: "contain",
+        }}
+        className="custom_divImage mt-4 relative w-full h-[50vh]"
+      >
+        <img
+          src={Pin}
+          alt="MNMap"
+          className="h-[50px] w-[50px] absolute"
+          style={{ top: "15%", right: "40%" }} // Adjust using percentages to scale correctly
+        />
+        <img
+          src={Pin}
+          alt="MNMap"
+          className="h-[50px] w-[50px] absolute"
+          style={{ top: "18%", right: "45%" }} // Adjust using percentages to scale correctly
+        />
+        <img
+          src={Pin}
+          alt="MNMap"
+          className="h-[50px] w-[50px] absolute"
+          style={{ top: "30%", right: "22%" }} // Adjust using percentages to scale correctly
+        />
+      </div>
     </ICard>
   );
 };
