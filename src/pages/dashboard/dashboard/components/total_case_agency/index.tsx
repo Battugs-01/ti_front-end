3ee15bfbe9e750d2259f56b16fc609ec773fc95e
Battugs Choijil <battugs@fibo.cloud>
2024-10-08 15:09:52 +0800

    [seccc]

--- src/pages/dashboard/dashboard/components/total_case_agency/index.tsx
+++ src/pages/dashboard/dashboard/components/total_case_agency/index.tsx
@@ -1,4 +1,5 @@
-import Pin from "assets/img/Pin.png";
+import Vector from "assets/img/Vector.svg";
+import { GenderBadge } from "components/badge/gender";
 import { ICard } from "components/card";
 import { FormattedMessage } from "react-intl";
 import { TotalPointInterface } from "service/dashboard/type";
@@ -13,34 +14,114 @@ export const TotalCaseAgency: React.FC<TotalCaseAgencyProps> = (data) => {
       <p className="px-5 text-xl font-semibold">
         <FormattedMessage id="total_case_agency" />
       </p>
-
-      <div
-        style={{
-          backgroundImage: "url('/svg/mn_map1.svg')",
-          backgroundPosition: "top", // Positions the image in the center
-          backgroundRepeat: "no-repeat", // Prevents the image from repeating
-          backgroundSize: "contain",
-        }}
-        className="custom_divImage mt-4 relative w-full h-[50vh]"
-      >
-        <img
-          src={Pin}
-          alt="MNMap"
-          className="h-[50px] w-[50px] absolute"
-          style={{ top: "15%", right: "40%" }} // Adjust using percentages to scale correctly
-        />
-        <img
-          src={Pin}
-          alt="MNMap"
-          className="h-[50px] w-[50px] absolute"
-          style={{ top: "18%", right: "45%" }} // Adjust using percentages to scale correctly
-        />
-        <img
-          src={Pin}
-          alt="MNMap"
-          className="h-[50px] w-[50px] absolute"
-          style={{ top: "30%", right: "22%" }} // Adjust using percentages to scale correctly
-        />
+      <div className="flex justify-center items-start">
+        <div
+          style={{
+            backgroundImage: "url('/svg/mn_map1.svg')",
+            backgroundPosition: "top",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100% 100%",
+            width: "676px",
+            height: "325px",
+          }}
+          className="custom_divImage mt-4 relative flex justify-between"
+        >
+          <div
+            style={{
+              backgroundImage: "url('/svg/pin.png')",
+              backgroundPosition: "top",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "100% 100%",
+              top: "50px",
+              right: "269px",
+            }}
+            className="h-[50px] w-[50px] absolute"
+          >
+            <div
+              className="flex justify-center items-center rounded-full bg-green-700 text-white text-[11px] p-1 m-2"
+              style={{
+                borderRadius: "50%",
+              }}
+            >
+              60%
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: "url('/svg/pin.png')",
+              backgroundPosition: "top",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "100% 100%",
+              top: "54px",
+              right: "233px",
+            }}
+            className="h-[50px] w-[50px] absolute"
+          >
+            <div
+              className="flex justify-center items-center rounded-full bg-blue-700 text-white text-[11px] p-1 m-2"
+              style={{
+                borderRadius: "50%",
+              }}
+            >
+              60%
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: "url('/svg/pin.png')",
+              backgroundPosition: "top",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "100% 100%",
+              top: "94px",
+              right: "253px",
+            }}
+            className="h-[50px] w-[50px] absolute"
+          >
+            <div
+              className="flex justify-center items-center rounded-full bg-[#f2ae00] text-white text-[11px] p-1 m-2"
+              style={{
+                borderRadius: "50%",
+              }}
+            >
+              60%
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row gap-4 justify-center mt-10">
+        <div className="flex gap-2">
+          <img src={Vector} alt="Vector" />
+          <div className="flex flex-col">
+            <div className="text-gray-700 font-medium text-base">Дархан</div>
+            <div className="flex gap-2">
+              <span className="text-[#7b878c] text-sm font-normal">60%</span>
+              <GenderBadge status="female" percent={45} />
+              <GenderBadge status="male" percent={55} />
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <img src={Vector} alt="Vector" />
+          <div className="flex flex-col">
+            <div className="text-gray-700 font-medium text-base">Дархан</div>
+            <div className="flex gap-2">
+              <span className="text-[#7b878c] text-sm font-normal">60%</span>
+              <GenderBadge status="female" percent={45} />
+              <GenderBadge status="male" percent={55} />
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <img src={Vector} alt="Vector" />
+          <div className="flex flex-col">
+            <div className="text-gray-700 font-medium text-base">Дархан</div>
+            <div className="flex gap-2">
+              <span className="text-[#7b878c] text-sm font-normal">60%</span>
+              <GenderBadge status="female" percent={45} />
+              <GenderBadge status="male" percent={55} />
+            </div>
+          </div>
+        </div>
       </div>
     </ICard>
   );
