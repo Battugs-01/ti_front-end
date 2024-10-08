ecef316d8584b3f7e82df2fdcf2b9f1bf9c75f1c
Battugs Choijil <battugs@fibo.cloud>
2024-10-08 10:34:30 +0800

    [devs]

--- src/pages/dashboard/dashboard/all/index.tsx
+++ src/pages/dashboard/dashboard/all/index.tsx
@@ -1,9 +1,10 @@
 import { useRequest } from "ahooks";
+import { notification } from "antd";
+import dashboard from "service/dashboard";
 import { QuestionList } from "../components/question_list";
 import { TotalCase } from "../components/total_case";
+import { TotalCaseAgency } from "../components/total_case_agency";
 import { TotalPoint } from "../components/total_point";
-import dashboard from "service/dashboard";
-import { notification } from "antd";
 
 export const All: React.FC = () => {
   const totalPoint = useRequest(dashboard.points, {
@@ -23,8 +24,11 @@ export const All: React.FC = () => {
   });
   return (
     <div className="grid lg:grid-cols-3 grid-cols-2 3xl:grid-cols-5 gap-6">
-      <div className="lg:col-span-2 3xl:col-span-4 col-span-2">
+      <div className="lg:col-span-2 3xl:col-span-2 col-span-2">
         <TotalPoint data={totalPoint.data} />
+      </div>
+      <div className="lg:col-span-2 3xl:col-span-2 col-span-2">
+        <TotalCaseAgency data={totalPoint.data} />
       </div>
       <div className="3xl:col-span-1 col-span-1 h-full">
         <TotalCase data={totalCase.data} />
--- src/pages/dashboard/dashboard/components/total_case_agency/index.tsx
+++ src/pages/dashboard/dashboard/components/total_case_agency/index.tsx
@@ -1,13 +1,18 @@
 import { ICard } from "components/card";
 import { FormattedMessage } from "react-intl";
+import { TotalPointInterface } from "service/dashboard/type";
 // import Map from "components/live-map/index.jsx";
 
-export const TotalCaseAgency: React.FC = () => {
+interface TotalCaseAgencyProps {
+  data?: TotalPointInterface[];
+}
+export const TotalCaseAgency: React.FC<TotalCaseAgencyProps> = (data) => {
   return (
     <ICard xR yR>
       <p className="px-5 text-xl font-semibold">
         <FormattedMessage id="total_case_agency" />
       </p>
+
       {/* <Map data={{}} /> */}
     </ICard>
   );
