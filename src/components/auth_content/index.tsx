94e9e04d105ac07f390fdb99f6dfee29d785e621
Bayarmunkh <bayarmunkh@fibo.cloud>
2024-10-18 14:41:23 +0800

    done

--- src/components/auth_content/index.tsx
+++ src/components/auth_content/index.tsx
@@ -5,26 +5,26 @@ import Logo from "/background/logo.svg";
 
 export const AuthContent: React.FC = () => {
   return (
-    <div className="col-span-3 hidden sm:flex flex-col p-12 gap-6 bg-[#DCE3E8] min-h-max h-full">
+    <div className="col-span-3 hidden sm:flex flex-col p-12 gap-6 bg-[#DCE3E8]">
       <div className="flex items-center justify-start mb-4">
         <img src={Logo} />
       </div>
-      <div className="text-primary-700 font-bold text-2xl lg:text-3xl 2xl:text-6xl text-center mx-10">
+      <div className="text-primary-700 font-bold text-2xl lg:text-3xl 2xl:text-4xl text-center mx-10">
         <FormattedMessage id="auth_title" />
       </div>
-      <div className="h-1/2 mb-5">
+      <div className="h-1/2 mb-10">
         <div className="relative">
           <img
             src={ContentOne}
-            className="absolute top-4 w-[320px] lg:w-[400px] 2xl:w-[550px]"
+            className="absolute top-4 w-[320px] lg:w-[400px] 2xl:w-[450px]"
           />
           <img
             src={ContentTwo}
-            className="absolute lg:top-10 top-40 right-10 w-[320px] lg:w-[400px] 2xl:w-[550px]"
+            className="absolute lg:top-10 top-40 right-10 w-[320px] lg:w-[400px] 2xl:w-[450px]"
           />
         </div>
       </div>
-      <footer className="text-[#416D74] text-xl lg:text-2xl text-center mt-32">
+      <footer className="text-[#416D74] text-xl lg:text-2xl text-center mt-20">
         © Developed by Quantum Partners LLC
       </footer>
     </div>
