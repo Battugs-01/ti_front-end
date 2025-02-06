6fbd19cb0531ea395dc8a6edcd39833e910b92e9
User <user@192.168.1.59>
2025-02-06 14:15:22 +0800

    [devs]

--- src/pages/dashboard/financiar/pages/CustomerCompany/actions/parts/info.tsx
+++ src/pages/dashboard/financiar/pages/CustomerCompany/actions/parts/info.tsx
@@ -90,7 +90,7 @@ export const Info: FC<IInfoProps> = ({ actionName }) => {
                   pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
                   message: "Энэ талбар и-мэйл хаяг байх ёстой",
                 },
-                ...FORM_ITEM_RULE(),
+                // ...FORM_ITEM_RULE(),
               ]}
             />
           </Col>
