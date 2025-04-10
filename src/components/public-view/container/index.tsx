016e50a03e75126fc4aa6d10ef17f367d36110fc
Choijil Battugs <choijilbattugs@Choijils-MacBook-Pro.local>
2025-04-10 19:21:28 +0800

    [hise]

--- src/components/public-view/container/index.tsx
+++ src/components/public-view/container/index.tsx
@@ -6,8 +6,7 @@ import ProForm, {
   ProFormSelect,
   ProFormText,
 } from "@ant-design/pro-form";
-import { Col, Row } from "antd";
-import { FORM_ITEM_RULE } from "config";
+import { Card, Col, Row } from "antd";
 import React, { useRef } from "react";
 import { CargoApproachList } from "service/feild_registration/type";
 import { CapacityOptions, DirectionOptions } from "utils/options";
@@ -20,235 +19,221 @@ const Container: React.FC<ContainerProps> = ({ data }) => {
   return (
     <ProForm initialValues={data} formRef={form} submitter={false}>
       <>
-        <Row gutter={[16, 16]}>
-          <Col span={4}>
-            <ProFormText
-              fieldProps={{
-                size: "large",
-              }}
-              name={"category"}
-              disabled
-              label={"Төрөл"}
-            />
-          </Col>
-          <Col span={10}>
-            <ProFormText
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name={"container_code"}
-              label={"Чингэлэг дугаар"}
-              rules={FORM_ITEM_RULE()}
-            />
-          </Col>
-          <Col span={10}>
-            <ProFormSelect
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name={["broker", "name"]}
-              label={"Зуучийн нэр"}
-              rules={FORM_ITEM_RULE()}
-            />
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col span={12}>
-            <ProFormSelect
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              options={CapacityOptions?.map((item) => ({
-                label: item.label,
-                value: item.value,
-              }))}
-              name={"capacity"}
-              label={"Даац"}
-              rules={FORM_ITEM_RULE()}
-            />
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col span={24}>
-            <ProFormRadio.Group
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name={"direction"}
-              label="Чиглэл"
-              options={DirectionOptions?.map((item) => ({
-                label: item.label,
-                value: item.value,
-              }))}
-              rules={FORM_ITEM_RULE()}
-            />
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col sm={12} xs={21}>
-            <ProFormDatePicker
-              name="approach_report_date"
-              fieldProps={{
-                size: "large",
-                onChange: (e) => {
-                  console.log(e, "lll");
-                },
-              }}
-              disabled
-              rules={FORM_ITEM_RULE()}
-              label="Дөхөлтийн мэдээний огноо"
-            />
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col span={12}>
-            <ProFormDatePicker
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name="arrived_at_site"
-              label="Т-д ирсэн"
-              rules={FORM_ITEM_RULE()}
-            />
-          </Col>
-          <Col span={12}>
-            <div className="flex items-center gap-3">
-              <ProFormDatePicker
+      <Card className="mb-3 h-full  bg-gray-50" title="Чингэлэг мэдээлэл">
+          <Row gutter={[16, 16]}>
+            <Col span={4}>
+              <ProFormText
                 fieldProps={{
                   size: "large",
                 }}
-                disabled
-                name={"opened_at"}
-                label="Задарсан"
-              />
-            </div>
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col span={12}>
-            <div className="flex items-center gap-3">
-              <ProFormDatePicker
+                name={"category"}
+                readonly
+                label={"Төрөл"}
+                />
+            </Col>
+            <Col span={10}>
+              <ProFormText
                 fieldProps={{
                   size: "large",
                 }}
-                disabled
-                name={"freed_at"}
-                label="Суларсан"
+                readonly
+                name={"container_code"}
+                label={"Чингэлэг дугаар"}
+                />
+            </Col>
+            <Col span={10}>
+                <ProFormSelect
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  readonly
+                  name={["broker", "name"]}
+                  label={"Зуучийн нэр"}
+                />
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+                <ProFormSelect
+                  fieldProps={{
+                    size: "large",
+                  }}
+                readonly
+                options={CapacityOptions?.map((item) => ({
+                  label: item.label,
+                  value: item.value,
+                }))}
+                name={"capacity"}
+                label={"Даац"}
+                />
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={24}>
+                <ProFormRadio.Group
+                  fieldProps={{
+                    size: "large",
+                  }}
+                readonly
+                name={"direction"}
+                label="Чиглэл"
+                options={DirectionOptions?.map((item) => ({
+                  label: item.label,
+                  value: item.value,
+                }))}
               />
-            </div>
-          </Col>
-          <Col span={12}>
-            <div className="flex items-center gap-3">
-              <ProFormDatePicker
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col sm={12} xs={21}>
+                <ProFormDatePicker
+                  name="approach_report_date"
+                fieldProps={{
+                  size: "large",
+                  onChange: (e) => {
+                    console.log(e, "lll");
+                  },
+                }}
+                readonly
+                label="Дөхөлтийн мэдээний огноо"
+              />
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+                <ProFormDatePicker
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  readonly
+                  name="arrived_at_site"
+                  label="Т-д ирсэн"
+              />
+            </Col>
+            <Col span={12}>
+                <ProFormDatePicker
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  readonly
+                  name={"opened_at"}
+                  label="Задарсан"
+                />
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+                <ProFormDatePicker
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  readonly
+                  name={"freed_at"}
+                  label="Суларсан"
+                />
+            </Col>
+            <Col span={12}>
+                <ProFormDatePicker
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  readonly
+                  name={"left_site_at"}
+                  label="Т-c явсан"
+                />
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+                <ProFormDatePicker
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  name={"returned_at"}
+                  readonly
+                  label="Буцаж ирсэн"
+                />
+            </Col>
+            <Col span={12}>
+                <ProFormDatePicker
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  name={"shipped_at"}
+                  readonly
+                  label="Ачилт хийсэн"
+                />
+            </Col>
+          </Row>
+        </Card>
+        <Card title="Олголт" className="h-full bg-gray-50">
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+                <ProFormText
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  readonly
+                  name={["assignation", "waggon_number"]}
+                  label="Вагоны дугаар"
+              />
+            </Col>
+            <Col span={12}>
+              <ProFormText
                 fieldProps={{
                   size: "large",
                 }}
-                disabled
-                name={"left_site_at"}
-                label="Т-c явсан"
+                readonly
+                name={["assignation", "shipping_number"]}
+                  label="Илгээлтийн дугаар"
+                />
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+                <ProFormText
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  readonly
+                  name={["assignation", "direction"]}
+                  label="Тээврийн чиглэл"
               />
-            </div>
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col span={12}>
-            <div className="flex items-center gap-3">
-              <ProFormDatePicker
+            </Col>
+            <Col span={12}>
+                <ProFormText
                 fieldProps={{
                   size: "large",
                 }}
-                name={"returned_at"}
-                disabled
-                label="Буцаж ирсэн"
-              />
-            </div>
-          </Col>
-          <Col span={12}>
-            <div className="flex items-center gap-3">
-              <ProFormDatePicker
+                readonly
+                name={["assignation", "cargo_name"]}
+                label="Ачааны нэр төрөл"
+                />
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+                <ProFormDigit
+                  fieldProps={{
+                    size: "large",
+                  }}
+                  readonly
+                  name={["assignation", "net_weight"]}
+                  label="Цэвэр жин"
+                />
+            </Col>
+            <Col span={12}>
+                <ProFormDigit
                 fieldProps={{
                   size: "large",
                 }}
-                name={"shipped_at"}
-                disabled
-                label="Ачилт хийсэн"
-              />
-            </div>
-          </Col>
-        </Row>
-        <div className="text-xl font-medium mb-3">Олголт</div>
-
-        <Row gutter={[16, 16]}>
-          <Col span={12}>
-            <ProFormText
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name={["assignation", "waggon_number"]}
-              label="Вагоны дугаар"
-            />
-          </Col>
-          <Col span={12}>
-            <ProFormText
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name={["assignation", "shipping_number"]}
-              label="Илгээлтийн дугаар"
-            />
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col span={12}>
-            <ProFormText
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name={["assignation", "direction"]}
-              label="Тээврийн чиглэл"
-            />
-          </Col>
-          <Col span={12}>
-            <ProFormText
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name={["assignation", "cargo_name"]}
-              label="Ачааны нэр төрөл"
-            />
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col span={12}>
-            <ProFormDigit
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name={["assignation", "net_weight"]}
-              label="Цэвэр жин"
-            />
-          </Col>
-          <Col span={12}>
-            <ProFormDigit
-              fieldProps={{
-                size: "large",
-              }}
-              disabled
-              name={["assignation", "gross_weight"]}
-              label="Бохир жин"
-            />
-          </Col>
-        </Row>
+                readonly
+                name={["assignation", "gross_weight"]}
+                  label="Бохир жин"
+                />
+            </Col>
+          </Row>
+        </Card>
       </>
     </ProForm>
   );
